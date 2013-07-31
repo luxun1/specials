3d72ffc42e53588c59afd4344776b12a1df6e74a
bradleybosher <bradleybosher@gmail.com>
2013-07-31 16:57:26 +0100

    Added in prices
    Added the original and reduced prices for reference

--- specials.lua
+++ specials.lua
@@ -1,17 +1,30 @@
 http = require "socket.http"
 require "socket"
-
+socket.http.TIMEOUT=360
 
 local output = assert(io.open("steam_specials.txt", "w"), "Failed to open output file")
 
-local games = ""
+local games = "Game\tOrignal Price\tReduced Price\n"
 
 local response = http.request("http://store.steampowered.com/search/?specials=1")
 
 
 for element in string.gmatch(response, "<h4>(.-)</h4>") do
 
-  games = games .. element .. "\n"
+
+
+  local start = string.find(response, element)
+
+	local original = string.match(response, "</h4>.-<strike>&#163;(.-)</strike>", start)
+
+	local reduced = string.match(response, "</strike>.-<br>&#163;(.-)</div>", start)
+
+	if element and original and reduced then
+
+		games = games .. element .. "\t£" .. original .. "\t£" .. reduced .. "\n"
+
+	end
+
 
 end
 
